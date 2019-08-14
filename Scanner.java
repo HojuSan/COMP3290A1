976b488aaf538cf29d381723c18963d64e8d8259
H0JuSan <ghwntks3@gmail.com>
2019-08-14 15:18:45 +1000

    Started on the scanner

---
+++ Scanner.java
@@ -0,0 +1,15 @@
+//* File:                       Main.java
+// * Course:                    COMP2240
+//  * Assignment:               Assignment3
+//   * Name:                    Juyong Kim  
+//    * Student Number:         c3244203
+//     * Purpose:               Main file
+//      * Note:                 Presumes all file na
+
+import java.io.*;
+import java.util.*;
+
+public class Scanner
+{
+
+}
