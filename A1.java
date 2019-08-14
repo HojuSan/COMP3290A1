968de5f35a8abc1c32e2ce0a5aeed2bdeee43133
H0JuSan <ghwntks3@gmail.com>
2019-08-14 16:14:10 +1000

    sort of understanding the scope of the assignment now

---
+++ A1.java
@@ -0,0 +1,15 @@
+//* File:                       A1.java
+// * Course:                    COMP2240
+//  * Assignment:               Assignment1
+//   * Name:                    Juyong Kim  
+//    * Student Number:         c3244203
+//     * Purpose:               Main file
+//      * Note:                 Presumes all file na
+
+import java.io.*;
+import java.util.*;
+
+public class A1
+{
+
+}
