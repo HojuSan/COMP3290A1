50c5700865c3ac0f2a581e770d7ab8b1c81e8418
H0JuSan <ghwntks3@gmail.com>
2019-08-25 01:31:44 +1000

    is getting to the output, currently confused how the scanner is going to communicate with the outputontroller though

--- A1.java
+++ A1.java
@@ -4,7 +4,7 @@
 //   * Name:                    Juyong Kim  
 //    * Student Number:         c3244203
 //     * Purpose:               Main file
-//      * Note:                 Presumes all file na
+//      * Note:                 very minimum is done here
 
 import java.io.*;
 import java.util.*;
@@ -13,7 +13,14 @@ public class A1
 {
 	public static void main(String args[])  throws IOException
 	{
-
+		File file = new File(args[0]);									//adds file
+		PrintWriter pw = new PrintWriter(System.out);					//Prints formatted representations of objects to a text-output stream
+		StringBuffer sb = new StringBuffer("");							//a string that can be modified
+		BufferedReader br = new BufferedReader(new FileReader(file));	//read char by char
+		OutputController output = new OutputController(br, pw, sb);
+		
+		//currently not sure how things will print
+		System.out.println();
+		pw.close();
 	}
-
 }
