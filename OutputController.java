08e5feb6ea264b4be93613b9014bce03a89bad47
H0JuSan <ghwntks3@gmail.com>
2019-08-25 00:55:07 +1000

    added output controller

---
+++ OutputController.java
@@ -0,0 +1,22 @@
+//* File:                       OutputController.java
+// * Course:                    COMP2240
+//  * Assignment:               Assignment1
+//   * Name:                    Juyong Kim  
+//    * Student Number:         c3244203
+//     * Purpose:               the lexical analyzer converts a sequence of characters into a sequence of 
+//      *                       distinct keywords, identifiers, constants, and delimiters.
+//       * Note:                Just SCANS doesn't do syntactic processing
+
+import java.io.*;
+import java.util.*;
+
+public class OutputController
+{
+
+
+    //constructor
+    public OutputController()
+    {
+
+    }
+}
